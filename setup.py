b628215d8520ff02b1102fb96f3d51bd2b551d9e
Owen W. Taylor <otaylor@fishsoup.net>
2020-07-15 14:19:51 -0400

    setup.py: Commit the version change to 0.11.5

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ else:
     entry_points = None
 
 setup(name='flatpak-module-tools',
-      version='0.11.4',
+      version='0.11.5',
       description='Tools for creating and maintaining Flatpaks as Fedora modules',
       url='https://pagure.io/flatpak-module-tools',
       author='Owen Taylor',
