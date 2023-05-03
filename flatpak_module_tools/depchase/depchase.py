ba180fd89792664cade3d3426ace9a7f04e91f51
Owen W. Taylor <otaylor@fishsoup.net>
2023-05-03 17:09:11 -0400

    depchase: remove funtionality we won't be using

--- flatpak_module_tools/depchase/depchase.py
+++ flatpak_module_tools/depchase/depchase.py
@@ -5,14 +5,13 @@ _depchase: Resolve dependency & build relationships between RPMs and SRPMs
 import collections
 import functools
 import logging
-import os
 import re
 
 import smartcols
 import solv
 
 from . import repodata
-from .util import arch_score, parse_dataset_name
+from .util import parse_dataset_name
 
 log = logging.getLogger(__name__)
 
@@ -69,22 +68,6 @@ def fix_deps(pool):
             s.unset(before)
             for dep in deps:
                 s.add_deparray(before, dep)
-
-
-def get_sourcepkg(p, s=None, only_name=False):
-    if s is None:
-        s = p.lookup_sourcepkg()[:-4]
-    if only_name:
-        return s
-    # Let's try to find corresponding source
-    sel = p.pool.select(s,
-                        solv.Selection.SELECTION_CANON
-                        | solv.Selection.SELECTION_SOURCE_ONLY)
-    sel.filter(p.repo.appdata.srcrepo.handle.Selection())
-    assert not sel.isempty(), "Could not find source package for {}".format(s)
-    solvables = sel.solvables()
-    assert len(solvables) == 1
-    return solvables[0]
 
 
 def _iterate_all_requires(package):
@@ -152,7 +135,6 @@ def print_transaction(details, pool):
     cl = tb.new_column("INFO")
     cl.tree = True
     cl_match = tb.new_column("MATCH")
-    cl_srpm = tb.new_column("SRPM")
     cl_repo = tb.new_column("REPO")
     for p in sorted(details):
         ln = tb.new_line()
@@ -171,7 +153,6 @@ def print_transaction(details, pool):
                     lnc = lnss
                     first = False
                 lnc[cl_match] = m
-                lnc[cl_srpm] = get_srpm_for_rpm(pool, m)
             sel = pool.select(m, solv.Selection.SELECTION_CANON)
             if sel.isempty():
                 lnc[cl_repo] = "Unknown repo"
@@ -233,18 +214,6 @@ def _solve(solver, pkgnames, full_info=False):
     return result
 
 
-def ensure_buildable(pool, pkgnames, full_info=False):
-    """Given a set of solvables, returns a set of source packages & build
-    deps"""
-    # The given package set may not be installable on its own
-    # That's OK, since other modules will provide those packages
-    # The goal of *this* method is to report the SRPMs that need to be
-    # built, and their build dependencies
-    sources = set(get_srpm_for_rpm(pool, n) for n in pkgnames)
-    builddeps = ensure_installable(pool, sources)
-    return sources, builddeps
-
-
 def make_pool(arch=None):
     return setup_pool(arch, repodata.setup_repos())
 
@@ -279,83 +248,22 @@ def ensure_installable(pool, pkgnames, hints=_DEFAULT_HINTS,
     return _solve(solver, pkgnames, full_info=full_info)
 
 
-def print_reldeps(pool, pkg):
-    sel = pool.select(pkg,
-                      solv.Selection.SELECTION_NAME
-                      | solv.Selection.SELECTION_DOTARCH)
-    assert not sel.isempty(), "Package can't be found"
-    found = sel.solvables()
-    assert len(found) == 1, "More matching solvables were found, {}".format(
-        found)
-    s = found[0]
-
-    reldep2str = {solv.SOLVABLE_REQUIRES: "requires",
-                  solv.SOLVABLE_RECOMMENDS: "recommends",
-                  solv.SOLVABLE_SUGGESTS: "suggests",
-                  solv.SOLVABLE_SUPPLEMENTS: "supplements",
-                  solv.SOLVABLE_ENHANCES: "enhances"}
-    for reltype, relstr in reldep2str.items():
-        for dep in s.lookup_deparray(reltype):
-            print("{}: {}".format(relstr, dep))
-
-
 def _get_rpm(pool, pkg):
-    search_criteria = (solv.Selection.SELECTION_NAME
-                    | solv.Selection.SELECTION_DOTARCH)
-    if "." in pkg:
-        search_criteria  |= solv.Selection.SELECTION_CANON
-    sel = pool.select(pkg, search_criteria)
+    sel = pool.select(pkg, solv.Selection.SELECTION_NAME | solv.Selection.SELECTION_DOTARCH)
     if sel.isempty():
-        raise RuntimeError("Couldn't find package {}".format(pkg))
+        raise RuntimeError(f"Couldn't find package {pkg}")
     found = sel.solvables()
 
-    # determine latest EVR in the found packages
-    found = sorted(found, key=functools.cmp_to_key(lambda a, b: b.evrcmp(a)))
-    latest_evr = found[0].evr
+    # Handle x86 32-bit vs 64-bit multilib packages
+    have_x86_64 = any(x.arch == "x86_64" for x in found)
+    have_i686 = any(x.arch == "i686" for x in found)
+    if have_x86_64 and have_i686:
+        found = [x for x in found if x.arch == "x86_64"]
 
-    # filter for latest and arch-compatible
-    found = [f for f in found if f.evr == latest_evr and arch_score(f.arch)]
-
-    # return the one package with the best matching architecture for multilib
-    return functools.reduce(lambda x, y:
-                            x if arch_score(x.arch) < arch_score(y.arch) else y,
-                            found)
+    found = sorted(found, key=functools.cmp_to_key(lambda a, b: a.evrcmp(b)))
+    return found[0]
 
 
 def get_srpm_for_rpm(pool, pkg):
-    s = _get_rpm(pool, pkg)
-
-    return get_sourcepkg(s, only_name=True)
-
-
-def get_rpms_for_srpms(pool, pkgnames):
-    sources = set()
-    for n in pkgnames:
-        sel = pool.select(n,
-                          solv.Selection.SELECTION_NAME
-                          | solv.Selection.SELECTION_DOTARCH
-                          | solv.Selection.SELECTION_SOURCE_ONLY)
-        if not sel.isempty():
-            srcrpm = sel.solvables()[0]
-            # lookup_location() here will be something like
-            #   Packages/n/nginx-1.12.1-1.fc27.src.rpm
-            # Which is closer to lookup_sourcepkg() which we match with below
-            sources.add(os.path.basename(srcrpm.lookup_location()[0]))
-
-    # This search is O(N) where N = the number of packages in Fedora
-    # so it would be nice to find a more algorithmically efficient approach
-    # OTOH, we only run this when *fetching* metadata, so it isn't too bad
-    result = set()
-    for p in (s for s in pool.solvables if s.arch not in ("src", "nosrc")):
-        if p.lookup_sourcepkg() in sources:
-            result.add(p.name)
-    return result
-
-
-def get_rpm_metadata(pool, pkg):
-    s = _get_rpm(pool, pkg)
-
-    return {
-        'summary': s.lookup_str(solv.SOLVABLE_SUMMARY),
-        'description': s.lookup_str(solv.SOLVABLE_DESCRIPTION),
-    }
+    solvable = _get_rpm(pool, pkg)
+    return solvable.lookup_sourcepkg()[:-4]
--- flatpak_module_tools/depchase/util/__init__.py
+++ flatpak_module_tools/depchase/util/__init__.py
@@ -1,4 +1,3 @@
-from .arch import score as arch_score  # noqa: F401
 from .dataset import display_dataset_name, get_default_dataset  # noqa: F401
 from .dataset import parse_dataset_name  # noqa: F401
 from .dict import DefaultDictWithKey, dict_merge_deep  # noqa: F401
