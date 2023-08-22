670cffd2bbc26117ab5ed6d2bf59515d0d262495
Owen W. Taylor <otaylor@fishsoup.net>
2023-08-22 00:39:23 -0400

    make-sources.sh: Add checking for files not in git
    Check for distributed files that aren't checked into git; and make
    the module location stricter not to pick up random python files.

--- make-sources.sh
+++ make-sources.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-set -ex
+set -e
+
+declare -A git_files
+while read -r filename ; do
+    git_files[$filename]=true
+done < <(git ls-tree -r --name-only HEAD)
+
+set -x
 
 pyproject-build -s --no-isolation --outdir .
 
@@ -9,6 +16,25 @@ RPM_VERSION=${PROJECT_VERSION/.post/^}
 if [[ $RPM_VERSION =~ ^([0-9.]+)((a|b|rc).*) ]] ; then
     RPM_VERSION=${BASH_REMATCH[1]}~${BASH_REMATCH[2]}
 fi
+
+# Check that we aren't distributing uncommitted files
+set +x
+while read -r filename ; do
+    filename=${filename#flatpak-module-tools-"${PROJECT_VERSION}"/}
+    if [[ $filename == "" ||
+          $filename =~ .*/$ ||
+          $filename == "PKG-INFO" ||
+          $filename == "VERSION" ||
+          $filename =~ flatpak_module_tools.egg-info/* ||
+          $filename == setup.cfg ]] ; then
+        continue
+    fi
+    if [[ $filename != "" && ${git_files[$filename]} == "" ]] ; then
+        echo "$filename is not in git"
+        exit 1
+    fi
+done < <(tar tf "flatpak-module-tools-$PROJECT_VERSION.tar.gz")
+set -x
 
 sed -E \
      -e 's/^(Version:[ 	]*).*/\1'"$RPM_VERSION"/ \
